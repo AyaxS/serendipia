d5ab5f73f245a3a2b14b1d8b2fda7872d5b512be
Christopher Sebastian Lara Cariceo <christopher.lara.c@gmail.com>
2020-05-08 00:57:45 -0400

    [global] agrega nueva seccion - iconos - slick carousel

--- js/app.js
+++ js/app.js
@@ -1,4 +1,11 @@
 $( document ).ready(function() {
     var windowHeight = $(window).height();
     $(".s_content-banner").css('height', windowHeight);
+
+    $('.your-class').slick({
+        slidesToShow: 3,
+        arrows: false,
+        dots:false,
+        centerMode: true
+    });
 });
