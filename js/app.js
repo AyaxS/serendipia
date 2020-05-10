4fc829c81ccf935362729e49e5ed5c4ddc53a3c5
Christopher Sebastian Lara Cariceo <christopher.lara.c@gmail.com>
2020-05-09 23:09:06 -0400

    [animate] agrega anclas de animacion en el menu del footer

--- js/app.js
+++ js/app.js
@@ -15,4 +15,12 @@ $( document ).ready(function() {
 		//fade: false,
         dots: false
     });
+
+    $('.ancla').click(function(e){				
+		e.preventDefault();		//evitar el eventos del enlace normal
+		var strAncla=$(this).attr('href'); //id del ancla
+        $('body,html').stop(true,true).animate({				
+            scrollTop: $(strAncla).offset().top
+        },1000);
+    });
 });
